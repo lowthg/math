ea1ae6895dca62d927745532204e3fcc7b5d8e82
George <george.lowther@blueyonder.co.uk>
2020-11-15 07:37:37 +0000

    coin density plot

--- bosonbiasedcoin.py
+++ bosonbiasedcoin.py
@@ -2,25 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def binomialprobs(n):
+def bosonprobs(n, p):
     yvals = np.zeros(n + 1)
-    prob = pow(0.5, n)
+    C = 0.0
+    prob = pow(1.0 - p, n)
+    a = p / (1.0 - p)
     for m in range(n+1):
         yvals[m] = prob
-        prob *= (n-m) / (m+1)
+        C += prob
+        prob *= a
+    yvals /= C
     return yvals
-
-
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.plot([0, 1], [1, 1], label='Uniform', linewidth=1)
-
 maxval = 1.0
-for n in [2, 10, 100, 500]:
+p = 0.49
+for n in [2, 50, 100, 200]:
     nvals = np.linspace(0.0, 1.0, n+1)
-    yvals = binomialprobs(n) * (n+1)
+    yvals = bosonprobs(n, p) * (n+1)
     ax.plot(nvals, yvals, label='N = {}'.format(n), linewidth=1)
     maxval = max(maxval, yvals.max())
 
