e9b11b939d7d44064c771ceadb43ffec9b7cae3b
George Lowther <37965159+lowthg@users.noreply.github.com>
2023-01-29 18:46:39 +0000

    choose w

--- polycommit.py
+++ polycommit.py
@@ -10,7 +10,7 @@ nbytes = 32
 nbits = nbytes * 8
 nmax = 2**nbits
 r = 32
-s = 10
+s = 30
 N = 2**s
 
 for k in range(1000):
@@ -21,51 +21,64 @@ for k in range(1000):
 else:
     raise "Could not find prime p"
 
-values = [random.randrange(p) for _ in range(N)]
-nonces = [random.randbytes(16) for _ in range(N)]
+for x in range(1000):
+    w = pow(x, (p-1)//N, p)
+    a = pow(w, N//2, p)
+    if a == p - 1:
+        print(x)
+        print(w)
+        break
+else:
+    raise "Could not find w"
 
-merkle = [None] * (s + 1)
-merkle[s] = [None] * N
-for i in range(N):
-    merkle[s][i] = sha256(values[i].to_bytes(16) + nonces[i]).digest()
+print("w = {}".format(hex(w)))
 
-for j in range(s, 0, -1):
-    M = 2**(j-1)
-    merkle[j - 1] = [None] * M
-    for i in range(M):
-        merkle[j-1][i] = sha256(merkle[j][2*i] + merkle[j][2*i + 1]).digest()
+if False:
+    values = [random.randrange(p) for _ in range(N)]
+    nonces = [random.randbytes(16) for _ in range(N)]
 
-root = merkle[0][0]
-print("Merkle root = {}".format(root.hex()))
+    merkle = [None] * (s + 1)
+    merkle[s] = [None] * N
+    for i in range(N):
+        merkle[s][i] = sha256(values[i].to_bytes(16) + nonces[i]).digest()
 
-index = int.from_bytes(sha256(root).digest()) & (N - 1)
+    for j in range(s, 0, -1):
+        M = 2**(j-1)
+        merkle[j - 1] = [None] * M
+        for i in range(M):
+            merkle[j-1][i] = sha256(merkle[j][2*i] + merkle[j][2*i + 1]).digest()
 
-print("Index = {}".format(index))
-print("      = {:010b}".format(index))
+    root = merkle[0][0]
+    print("Merkle root = {}".format(root.hex()))
 
-value = values[index]
-nonce = nonces[index]
-print("Value = {}".format(value.to_bytes(16).hex()))
-print("Nonce = {}".format(nonce.hex()))
+    index = int.from_bytes(sha256(root).digest()) & (N - 1)
 
-proof = []
-for j in range(s, 0, -1):
-    i = (index >> (s - j)) ^ 1
-    proof.append(merkle[j][i])
+    print("Index = {}".format(index))
+    print("      = {:010b}".format(index))
 
-print("Merkle proof = [")
-for i in range(len(proof)):
-    print("    {}{}".format(proof[i].hex(), ',' if i < s - 1 else ''))
-print("]")
+    value = values[index]
+    nonce = nonces[index]
+    print("Value = {}".format(value.to_bytes(16).hex()))
+    print("Nonce = {}".format(nonce.hex()))
 
-h = sha256(value.to_bytes(16) + nonce).digest()
-print('h = SHA256(value|nonce)')
-for i in range(s):
-    if index & (1 << i) == 0:
-        h = sha256(h + proof[i]).digest()
-        print('h = SHA256(h|mp[{}])'.format(i))
-    else:
-        h = sha256(proof[i] + h).digest()
-        print('h = SHA256(mp[{}]|h)'.format(i))
+    proof = []
+    for j in range(s, 0, -1):
+        i = (index >> (s - j)) ^ 1
+        proof.append(merkle[j][i])
 
-print('  = {}'.format(h.hex()))
+    print("Merkle proof = [")
+    for i in range(len(proof)):
+        print("    {}{}".format(proof[i].hex(), ',' if i < s - 1 else ''))
+    print("]")
+
+    h = sha256(value.to_bytes(16) + nonce).digest()
+    print('h = SHA256(value|nonce)')
+    for i in range(s):
+        if index & (1 << i) == 0:
+            h = sha256(h + proof[i]).digest()
+            print('h = SHA256(h|mp[{}])'.format(i))
+        else:
+            h = sha256(proof[i] + h).digest()
+            print('h = SHA256(mp[{}]|h)'.format(i))
+
+    print('  = {}'.format(h.hex()))
