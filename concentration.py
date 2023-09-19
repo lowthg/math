a1d66276042a92c0aca8d16ffbe71646fde6a9f4
George Lowther <37965159+lowthg@users.noreply.github.com>
2023-09-19 21:14:04 +0100

    tidy

--- concentration.py
+++ concentration.py
@@ -17,41 +17,42 @@ r3 = math.sqrt(3)
 plt.rcParams.update({"text.usetex": True})
 fig = plt.figure()
 ax = fig.add_subplot(111)
-xmax = 1.2
 col1 = "blue"
 
 marker1 = dict(marker='o', markerfacecolor='white', markeredgecolor=col1)
 marker2 = dict(marker='o', markerfacecolor=col1, markeredgecolor=col1)
 
-ax.plot([0], [1], linewidth=2, color=col1, **marker2, clip_on=False, zorder=3, label='optimal bound')
-ax.plot([0, 1 / r7], [1 / 2, 1 / 2], linewidth=2, color=col1, **marker1, clip_on=False, zorder=3)
-ax.plot([1 / r7], [1/2], linewidth=2, color=col1, **marker2, zorder=3)
-ax.plot([1 / r7, 1 / r5], [29 / 64, 29 / 64], linewidth=2, color=col1, **marker1)
-ax.plot([1 / r5], [29/64], linewidth=2, color=col1, **marker2)
-ax.plot([1 / r5, 1 / r3], [3 / 8, 3 / 8], linewidth=2, color=col1, **marker1)
-ax.plot([1 / r3], [3/8], linewidth=2, color=col1, **marker2)
-ax.plot([1 / r3, 2 / r6], [1 / 4, 1 / 4], linewidth=2, color=col1, **marker1)
-ax.plot([2 / r6], [1 / 4], linewidth=2, color=col1, **marker2)
-ax.plot([2/r6, 1], [7/32, 7/32], linewidth=2, color=col1, **marker1)
-ax.plot([1], [7/32], linewidth=2, color=col1, **marker2)
-ax.plot([1, xmax], [0, 0], linewidth=2, color=col1, clip_on=False, zorder=3)
-ax.plot([1], [0], linewidth=2, color=col1, **marker1, clip_on=False, zorder=3)
+concentration=True
 
-xvals = np.linspace(0, xmax, 200)
-yvals = [stats.norm.cdf(-x) * 2 for x in xvals]
-xvals2 = np.linspace(0, 1, 200)
-yvals2 = [(1-x*x)**2/3 for x in xvals2]
-ax.plot(xvals, yvals, linestyle='dashed', color='grey', label='Gaussian bound')
-ax.plot(xvals2, yvals2, linestyle='dashed', color='green', label='$(1-x^2)^2/3$')
-ax.legend(loc='upper right')
+if concentration:
+    xmax = 1.2
+    ax.plot([0], [1], linewidth=2, color=col1, **marker2, clip_on=False, zorder=3, label='optimal bound')
+    ax.plot([0, 1 / r7], [1 / 2, 1 / 2], linewidth=2, color=col1, **marker1, clip_on=False, zorder=3)
+    ax.plot([1 / r7], [1/2], linewidth=2, color=col1, **marker2, zorder=3)
+    ax.plot([1 / r7, 1 / r5], [29 / 64, 29 / 64], linewidth=2, color=col1, **marker1)
+    ax.plot([1 / r5], [29/64], linewidth=2, color=col1, **marker2)
+    ax.plot([1 / r5, 1 / r3], [3 / 8, 3 / 8], linewidth=2, color=col1, **marker1)
+    ax.plot([1 / r3], [3/8], linewidth=2, color=col1, **marker2)
+    ax.plot([1 / r3, 2 / r6], [1 / 4, 1 / 4], linewidth=2, color=col1, **marker1)
+    ax.plot([2 / r6], [1 / 4], linewidth=2, color=col1, **marker2)
+    ax.plot([2/r6, 1], [7/32, 7/32], linewidth=2, color=col1, **marker1)
+    ax.plot([1], [7/32], linewidth=2, color=col1, **marker2)
+    ax.plot([1, xmax], [0, 0], linewidth=2, color=col1, clip_on=False, zorder=3)
+    ax.plot([1], [0], linewidth=2, color=col1, **marker1, clip_on=False, zorder=3)
 
-#ax.plot([0, 0,   1/r7, 1/r7,  1/r5,  1/r5, 1/r3, 1/r3, 2/r6, 2/r6, 1,    1, 2],
-#        [1, 1/2, 1/2,  29/64, 29/64, 3/8,  3/8,  1/4,  1/4,  7/32, 7/32, 0, 0])
+    xvals = np.linspace(0, xmax, 200)
+    yvals = [stats.norm.cdf(-x) * 2 for x in xvals]
+    xvals2 = np.linspace(0, 1, 200)
+    yvals2 = [(1-x*x)**2/3 for x in xvals2]
+    ax.plot(xvals, yvals, linestyle='dashed', color='grey', label='Gaussian bound')
+    ax.plot(xvals2, yvals2, linestyle='dashed', color='green', label='$(1-x^2)^2/3$')
+
+    ax.set_xticks([0, 1/r7, 1/r5, 1/r3, 2/r6, 1], [0, '$\\frac1{\\sqrt7}$', '$\\frac1{\\sqrt5}$', '$\\frac1{\\sqrt3}$',
+                                                   '$\\frac2{\\sqrt6}$', 1])
+    ax.set_yticks([0, 7/32, 1/4, 3/8, 29/64, 1/2, 1], [0, '7/32', '1/4', '3/8', '29/64', '1/2', 1])
+
 ax.set_xlim(0, xmax)
 ax.set_ylim(0, 1)
-ax.set_xticks([0, 1/r7, 1/r5, 1/r3, 2/r6, 1], [0, '$\\frac1{\\sqrt7}$', '$\\frac1{\\sqrt5}$', '$\\frac1{\\sqrt3}$',
-                                               '$\\frac2{\\sqrt6}$', 1])
-ax.set_yticks([0, 7/32, 1/4, 3/8, 29/64, 1/2, 1], [0, '7/32', '1/4', '3/8', '29/64', '1/2', 1])
 #ax.set_yticks([])
 #plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99, hspace=0, wspace=0)
 plt.show()
