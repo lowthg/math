258801c53e7784aa5eef20b32d0a335480cca29f
George Lowther <37965159+lowthg@users.noreply.github.com>
2024-05-16 23:03:42 +0100

    manim

--- ABRACADABRA/scene.py
+++ ABRACADABRA/scene.py
@@ -7,3 +7,143 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, DOWN, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(Transform(square, circle))  # transform the square into a circle
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+        self.play(FadeOut(square))  # fade out animation
+
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI / 2), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+
+class TwoTransforms(Scene):
+    def transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(Transform(a, b))
+        self.play(Transform(a, c))
+        self.play(FadeOut(a))
+
+    def replacement_transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(ReplacementTransform(a, b))
+        self.play(ReplacementTransform(b, c))
+        self.play(FadeOut(c))
+
+    def construct(self):
+        self.transform()
+        self.wait(0.5)  # wait for 0.5 seconds
+        self.replacement_transform()
+
+
+class TransformCycle(Scene):
+    def construct(self):
+        a = Circle()
+        t1 = Square()
+        t2 = Triangle()
+        self.add(a)
+        self.wait()
+        for t in [t1, t2]:
+            self.play(Transform(a, t))
+
+
+class HelloWorld(Scene):
+    def construct(self):
+        text = Text("Hello world", font_size=144)
+        self.add(text)
+
+
+class SingleLineColor(Scene):
+    def construct(self):
+        text = MarkupText(
+            f'all in red <span fgcolor="{YELLOW}">except this</span>', color=RED
+        )
+        self.add(text)
+
+
+class DifferentWeight(Scene):
+    def construct(self):
+        import manimpango
+
+        g = VGroup()
+        weight_list = dict(
+            sorted(
+                {
+                    weight: manimpango.Weight(weight).value
+                    for weight in manimpango.Weight
+                }.items(),
+                key=lambda x: x[1],
+            )
+        )
+        for weight in weight_list:
+            g += Text(weight.name, weight=weight.name, font="Arial")
+        self.add(g.arrange(DOWN).scale(0.5))
+
+
+class MonkeyType(Scene):
+    def construct(self):
+        ft = Text("THE INFINITE MONKEY THEOREM", font="Courier New", weight=SEMIBOLD, color=BLUE, font_size=30).move_to(
+            TOP)
+        ft2 = Text("A monkey hitting keys at random, on a typewriter\n"
+                   "keyboard for an infinite amount of time will almost\n"
+                   "surely type any given text, including the complete\n"
+                   "works of William Shakespeare.", font="Courier New", font_size=30, line_spacing=1.2)
+
+        ft2.next_to(ft, DOWN, buff=0.5)
+        self.add(ft, ft2)
+
+
+class AddPackageLatex(Scene):
+    def construct(self):
+        myTemplate = TexTemplate()
+        myTemplate.add_to_preamble(r"\usepackage{mathrsfs}")
+        tex = Tex(
+            r"$\mathscr{H} \rightarrow \mathbb{H}$",
+            tex_template=myTemplate,
+            font_size=144,
+        )
+        self.add(tex)
