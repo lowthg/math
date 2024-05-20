73449a9b9f2e7d866201f91c93215495a0cb3fdb
George Lowther <37965159+lowthg@users.noreply.github.com>
2024-05-20 01:05:20 +0100

    move code

--- manim/project/scene.py
+++ manim/project/scene.py
@@ -124,46 +124,6 @@ class DifferentWeight(Scene):
         self.add(g.arrange(DOWN).scale(0.5))
 
 
-class MonkeyType(Scene):
-    def construct(self):
-        monkey = ImageMobject("Chimpanzee_seated_at_typewriter.jpg").scale(0.7).to_edge(DR, buff=0.1)
-        ft = Text("THE INFINITE MONKEY THEOREM", font="Courier New", weight=SEMIBOLD, color=BLUE, font_size=30)\
-            .to_edge(UP, buff=1).shift(LEFT).set_opacity(0)
-        bodytext = "\tA monkey hitting keys at random on a\n"\
-                   "typewriter keyboard, for an infinite amount\n"\
-                   "of time, will almost surely type any given\n"\
-                   "text, including the complete\n"\
-                   "works of William Shakespeare."
-        ft2 = Text(bodytext, font="Courier New", font_size=30, line_spacing=1.2).set_opacity(0)
-
-        ias = ft2.text.find('almost')
-
-        ft2.next_to(ft, DOWN, buff=0.5)
-
-        self.add(monkey)
-
-        self.add(ft, ft2)
-
-        if True:
-            for x in ft:
-                self.wait(0.12)
-                x.set_opacity(1)
-
-            for x in ft2:
-                self.wait(0.12)
-                x.set_opacity(1)
-        else:
-            ft.set_opacity(1)
-            ft2.set_opacity(1)
-            self.wait(1)
-
-        self.play(ft2[ias:ias+10].animate.set(color="#E02A20").set_style(stroke_width=1.2))
-        self.play(ft.animate.set_opacity(0), ft2[:ias].animate.set_opacity(0), ft2[ias+10:].animate.set_opacity(0),
-                  monkey.animate.set_opacity(0), run_time=3)
-        self.play(ft2.animate.set(font_size=100).shift(RIGHT*2.6))
-        self.wait(1)
-
-
 class MathTeXDemo(Scene):
     def construct(self):
         rtarrow0 = MathTex(r"\xrightarrow{x^6y^8}", font_size=96)
