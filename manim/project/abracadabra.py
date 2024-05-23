ae097c4980acb50e3e0f74039122c70318a42ef6
George Lowther <37965159+lowthg@users.noreply.github.com>
2024-05-23 19:39:16 +0100

    geometric

--- manim/project/abracadabra.py
+++ manim/project/abracadabra.py
@@ -3,6 +3,7 @@ Animations for ABRACADABRA! The magic of martingale theory
 """
 
 from manim import *
+import numpy as np
 import math
 import random
 
@@ -116,18 +117,203 @@ class SequenceH(Scene):
             'H',
             'TTH'
         ]
-        coins = VGroup(*[VGroup(*[get_coin(face) for face in row]).arrange(RIGHT) for row in sequences]).arrange(DOWN).to_edge(UL)
+        coins = VGroup(*[
+            VGroup(*[get_coin(face) for face in row]).arrange(RIGHT) for row in sequences
+        ]).arrange(DOWN).to_edge(UL)
         for row in coins:
-            row.to_edge(LEFT)
+            row.to_edge(LEFT).shift(RIGHT)
+
+        for row in coins:
+            self.wait(0.5)
             for coin in row:
                 animate_flip(self, coin)
-            nh = MathTex('N_H={}'.format(len(row)), font_size=60).next_to(coins, RIGHT).set_y(row.get_y())
-            self.add(nh)
-            self.wait(1)
+            self.wait(0.5)
+            nh = MathTex('N_H={}'.format(len(row)), font_size=80).next_to(coins, RIGHT, buff=1).set_y(row.get_y())
+            self.play(FadeIn(nh))
+            self.wait(0.5)
+
+        eq1 = MathTex(r'\mathbb E[N_H]=2', font_size=80).to_edge(DOWN, buff=1.2)
+        self.play(FadeIn(eq1))
+        self.wait(1)
+
+
+class GeometricMean(Scene):
+    def construct(self):
+        font_size = 60
+
+        self.wait(0.1)
+        coins = VGroup(*[get_coin(face) for face in 'TTTTH']).arrange(RIGHT).to_edge(UP)
+        for coin in coins:
+            animate_flip(self, coin)
+
+        txt0 = VGroup(Tex(r'Biased coin with $\mathbb P({\rm H})=p$', font_size=60),
+                      Tex(r'$N_H=$ number of tosses to get H', font_size=60))\
+            .arrange(DOWN, center=False, aligned_edge=LEFT).to_edge(UL).shift(DOWN*1.4)
+
+        self.play(FadeIn(txt0[0]), run_time=1)
+        self.play(FadeIn(txt0[1]), run_time=1)
+
+        eq1 = MathTex(r'\mathbb P(N_H > n){{=}}\mathbb P({{{\rm first\ }n{\rm\ tosses\ are\ T}}})',
+                      font_size=font_size).next_to(txt0, DOWN, buff=1).align_to(txt0, LEFT)
+        self.play(FadeIn(eq1), run_time=0.5)
+
+        eq2 = MathTex(r'{{=}}\mathbb P({{1^{\rm st}{\rm\ is\ T}}},{{2^{\rm nd}{\rm\ is\ T}}},\ldots,{{n^{\rm th}{\rm\ is\ T}}})',
+                      font_size=font_size)
+        eq2.shift(eq1[1].get_center()-eq2[0].get_center())
+        self.wait(0.5)
+        self.play(ReplacementTransform(eq1[1:3], eq2[0:2]),
+                  #ReplacementTransform(eq1[4], eq2[7]),
+                  run_time=0.5)
+        self.play(FadeOut(eq1[3:5]),
+                  FadeIn(eq2[2:8]),
+                  run_time=2)
+        self.wait(0.5)
+
+        eq3 = MathTex(r'{{=}}\mathbb P({{1^{\rm st}{\rm\ is\ T}}})\mathbb P({{2^{\rm nd}{\rm\ is\ T}}})\cdots\mathbb P({{n^{\rm th}{\rm\ is\ T}}})',
+                      font_size=font_size)
+        eq3.shift(eq2[0].get_center()-eq3[0].get_center())
+
+        txt1 = Text(r'Independence!', color=RED, font_size=font_size).next_to(eq3, DOWN).align_to(eq3[1])
+        self.add(txt1)
+        self.play(ReplacementTransform(eq2[0:3], eq3[0:3]),
+                  ReplacementTransform(eq2[4], eq3[4]),
+                  ReplacementTransform(eq2[6:8], eq3[6:8]),
+                  FadeOut(eq2[3], eq2[5]),
+                  FadeIn(eq3[3], eq3[5]),
+                  run_time=2)
+        self.remove(txt1)
+
+        self.wait(0.5)
+
+        eq4 = MathTex(r'\mathbb P(N_H > n){{=}}(1-p)^n', font_size=font_size)
+        eq4.shift(eq3[0].get_center() - eq4[1].get_center())
+
+        eq4_1 = eq4[2][0:5].copy()
+        eq4_2 = eq4[2][0:5].copy()
+        eq4_3 = eq4[2][0:5].copy()
+        eq4_1.shift([(eq3[2].get_center() - eq4_1.get_center())[0], 0, 0])
+        eq4_2.shift([(eq3[4].get_center() - eq4_2.get_center())[0], 0, 0])
+        eq4_3.shift([(eq3[6].get_center() - eq4_3.get_center())[0], 0, 0])
+
+        self.play(FadeOut(eq3[1][0], eq3[2], eq3[3][1], eq3[4], eq3[5][4], eq3[6]),
+                  ReplacementTransform(eq3[1][1], eq4_1[0]),
+                  ReplacementTransform(eq3[3][0], eq4_1[-1]),
+                  ReplacementTransform(eq3[3][2], eq4_2[0]),
+                  ReplacementTransform(eq3[5][0], eq4_2[-1]),
+                  ReplacementTransform(eq3[5][-1], eq4_3[0]),
+                  ReplacementTransform(eq3[7][0], eq4_3[-1]),
+                  FadeIn(eq4_1[1:-1], eq4_2[1:-1], eq4_3[1:-1]),
+                  run_time=2)
+
+        self.wait(0.5)
+
+        self.play(ReplacementTransform(eq3[0], eq4[1]),
+                  ReplacementTransform(eq4_1[0:5], eq4[2][0:5]),
+                  ReplacementTransform(eq4_2[0:5], eq4[2][0:5]),
+                  ReplacementTransform(eq4_3[0:5], eq4[2][0:5]),
+                  FadeOut(eq3[5][1:-2]),
+                  FadeIn(eq4[2][-1]),
+                  ReplacementTransform(eq1[0], eq4[0]),
+                  run_time=2)
+
+        self.wait(0.5)
+
+        txt2 = Text(r'Geometric distribution!', color=RED, font_size=font_size).next_to(eq4, DOWN).move_to(ORIGIN, coor_mask=np.array([1,0,0]))
+        self.play(FadeIn(txt2), run_time=0.1)
+        self.wait(1)
+        self.play(FadeOut(txt2), run_time=0.1)
+        self.wait(0.5)
+
+        self.play(eq4.animate.next_to(txt0, DOWN).align_to(txt0, LEFT))
 
         self.wait(1)
 
-#SequenceH().construct()
+        eq5 = MathTex(r'\mathbb E[N_H]{{=}}{{\sum_{m=0}^\infty}}m{{\mathbb P(N_H = m)}}', font_size=font_size)\
+            .next_to(eq4, DOWN).align_to(eq4, LEFT)
+        self.play(FadeIn(eq5), run_time=0.5)
+        self.wait(0.5)
+
+        eq6 = MathTex(r'{{=}}{{\sum_{m=0}^\infty}}\sum_{n=0}^{m-1}{{\mathbb P(N_H = m)}}', font_size=font_size)
+        eq6.shift(eq5[1].get_center() - eq6[0].get_center())
+        self.play(ReplacementTransform(eq5[2], eq6[1]),
+                 # ReplacementTransform(eq5[3], eq6[2]),
+                  ReplacementTransform(eq5[3][0], eq6[2][0]),
+                  FadeIn(eq6[2][1:]),
+                  ReplacementTransform(eq5[4], eq6[3]),
+                  run_time=2)
+        self.wait(0.5)
+        eq7 = MathTex(r'{{=}}{{\sum_{n=0}^\infty}}\sum_{m=n+1}^\infty{{\mathbb P(N_H = m)}}', font_size=font_size)
+        eq7.shift(eq6[0].get_center() - eq7[0].get_center())
+        self.play(ReplacementTransform(eq6[0], eq7[0]),
+                  ReplacementTransform(eq6[2][3:], eq7[1][1:]),
+                  ReplacementTransform(eq6[2][:3], eq7[1][:1]),
+                  ReplacementTransform(eq6[1][:4], eq7[2][:4]),
+                  ReplacementTransform(eq6[1][4:], eq7[2][4:]),
+                  ReplacementTransform(eq6[3], eq7[3]),
+                  run_time=2)
+        self.wait(0.5)
+        eq8 = MathTex(r'{{=}}{{\sum_{n=0}^\infty}}{{\mathbb P(N_H > n)}}', font_size=font_size)
+        eq8.shift(eq7[0].get_center() - eq8[0].get_center())
+        self.play(ReplacementTransform(eq7[:2], eq8[:2]),
+                  FadeOut(eq7[2]),
+                  ReplacementTransform(eq7[3], eq8[2]),
+                  run_time=2)
+        self.wait(0.5)
+
+        eq9 = MathTex(r'{{=}}{{\sum_{n=0}^\infty}}{{(1-p)^n}}', font_size=font_size)
+        eq9.shift(eq8[0].get_center() - eq9[0].get_center())
+        self.play(ReplacementTransform(eq8[:2], eq9[:2]),
+                  ReplacementTransform(eq4[2].copy(), eq9[2]),
+                  FadeOut(eq8[2]),
+                  run_time=2)
+        self.wait(0.5)
+
+        eq10 = MathTex(r'={{\frac{1}{1-(1-p)}}}', font_size=font_size)
+        eq10.shift(eq9[0].get_center() - eq10[0].get_center())
+        txt2 = Text(r'Geometric series!', color=RED, font_size=font_size).next_to(eq10, RIGHT, buff=1)
+        self.add(txt2)
+        self.play(FadeOut(eq9[1:2], eq9[2][-1]),
+                  FadeIn(eq10[1][:4]),
+                  ReplacementTransform(eq9[2][:-1], eq10[1][4:]),
+                  ReplacementTransform(eq9[0], eq10[0]),
+                  run_time=2)
+        self.remove(txt2)
+        self.wait(0.5)
+
+        eq11 = MathTex(r'={{\frac{1}{1-(1+p)}}}', font_size=font_size)
+        eq11.shift(eq10[0].get_center() - eq11[0].get_center())
+        self.play(ReplacementTransform(eq10[0], eq11[0]),
+                  ReplacementTransform(eq10[1][:4], eq11[1][:4]),
+                  ReplacementTransform(eq10[1][5:8], eq11[1][5:8]),
+                  FadeOut(eq10[1][4], eq10[1][8]),
+                  run_time=0.5)
+
+        eq12 = MathTex(r'={{\frac{1}{1-1+p}}}', font_size=font_size)
+        eq12.shift(eq11[0].get_center() - eq12[0].get_center())
+        self.play(ReplacementTransform(eq11[0], eq12[0]),
+                  ReplacementTransform(eq11[1][:4], eq12[1][:4]),
+                  ReplacementTransform(eq11[1][5:8], eq12[1][4:7]),
+                  run_time=1)
+        self.wait(0.5)
+
+        l1 = Line(LEFT*0.4, RIGHT*0.4, color=BLUE, stroke_width=10).move_to(eq12[1][2]).rotate(0.8)
+        l2 = Line(LEFT*0.4, RIGHT*0.4, color=BLUE, stroke_width=10).move_to(eq12[1][4]).rotate(0.8)
+        self.play(FadeIn(l1, l2))
+        self.wait(0.5)
+
+        self.play(FadeOut(l1, l2, eq12[1][2:5]), run_time=1)
+        self.wait(0.5)
+
+        eq13 = MathTex(r'\mathbb E[N_H]{{=}}\frac1p', font_size=font_size)
+        eq13.shift(eq12[0].get_center() - eq13[1].get_center())
+        self.play(ReplacementTransform(eq5[0], eq13[0]),
+                  ReplacementTransform(eq12[0], eq13[1]),
+                  ReplacementTransform(eq12[1][:2], eq13[2][:2]),
+                  ReplacementTransform(eq12[1][6], eq13[2][2]),
+                  FadeOut(eq12[1][5]),
+                  run_time=1)
+        self.wait(2)
+
 
 class Teaser2(Scene):
     def construct(self):
