c3c1f8dadf373b01f1bc48c05c95a1e66d3a2014
George Lowther <37965159+lowthg@users.noreply.github.com>
2024-06-06 20:02:26 +0100

    continued fractions

--- geometricCF.py
+++ geometricCF.py
@@ -14,36 +14,52 @@ fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 ax.axis('off')
 height = 1
-width = math.pi
+width = math.exp(1)
 
 ax.set_xlim(0, width)
-ax.set_ylim(0, 1)
+ax.set_ylim(0, height)
 ax.set_xticks([])
 ax.set_yticks([])
-plt.subplots_adjust(left=0.0, right=1, bottom=0.0, top=1, hspace=0, wspace=0)
 
 
 x0, x1, y0, y1 = 0, width, 0, 1
 
 colors = [_ for _ in matplotlib.colors.TABLEAU_COLORS.keys()]
 i = 0
+j = 0
+dir = None
 
+lw = [1, 1, 1, 1, 0.5, 0.5, 0.1, 0.1, 0.05, 0.02, 0.02, 0.05, 0.01]
 while True:
-    color = colors[i]
-    i = (i + 1) % len(colors)
+    color = colors[i % len(colors)]
     width = x1 - x0
     height = y1 - y0
-    if min(width, height) < 0.001:
+    if min(width, height) < 0.00001:
         break
     if width > height:
         w = height
         x, y = x0, y0
         x0 += w
+        if dir == 1:
+            j += 1
+        dir = 0
     else:
         w = width
         x, y = x0, y1 - w
         y1 -= w
-    r = matplotlib.patches.Rectangle((x, y), w, w, fill=True, lw=1, color='black', fc=color)
+        if dir == 0:
+            j += 1
+        dir = 1
+    r = matplotlib.patches.Rectangle((x, y), w, w, fill=True, lw=lw[min(i, len(lw)-1)], color='black', fc=color)
     ax.add_patch(r)
+    i += 1
+
+print(j)
+
+plt.tight_layout(pad=0, w_pad=0, h_pad=0)
+plt.subplots_adjust(left=0.0, right=1, bottom=0.0, top=1, hspace=0, wspace=0)
+print('saving...')
+plt.savefig('plot.png', dpi=3000, bbox_inches=0, pad_inches=0)
+print('saved')
 
 plt.show()
