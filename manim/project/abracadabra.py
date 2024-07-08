62e22df10e8365274e7abbe882172a4e908c223d
George Lowther <37965159+lowthg@users.noreply.github.com>
2024-07-08 04:14:37 +0100

    improve

--- manim/project/abracadabra.py
+++ manim/project/abracadabra.py
@@ -158,13 +158,13 @@ class MonkeyType(Scene):
     def construct(self):
         monkey = ImageMobject("Chimpanzee_seated_at_typewriter.jpg").scale(0.7).to_edge(DR, buff=0.1)
         ft = Text("THE INFINITE MONKEY THEOREM", font="Courier New", weight=SEMIBOLD, color=BLUE, font_size=30)\
-            .to_edge(UP, buff=1).shift(LEFT).set_opacity(0)
+            .to_edge(UP, buff=1).shift(LEFT)
         bodytext = "\tA monkey hitting keys at random on a\n"\
                    "typewriter keyboard, for an infinite amount\n"\
                    "of time, will almost surely type any given\n"\
                    "text, including the complete\n"\
                    "works of William Shakespeare."
-        ft2 = Text(bodytext, font="Courier New", font_size=30, line_spacing=1.2).set_opacity(0)
+        ft2 = Text(bodytext, font="Courier New", font_size=30, line_spacing=1.2)
 
         ias = ft2.text.find('almost')
 
@@ -172,25 +172,29 @@ class MonkeyType(Scene):
 
         self.add(monkey)
 
-        self.add(ft, ft2)
+#        self.add(ft, ft2)
+        self.wait(0.5)
 
         if True:
             for x in ft:
-                self.wait(0.12)
-                x.set_opacity(1)
+                self.wait(0.07)
+#                x.set_opacity(1)
+                self.add(x)
 
             for x in ft2:
-                self.wait(0.12)
-                x.set_opacity(1)
+                self.wait(0.07)
+#                x.set_opacity(1)
+                self.add(x)
         else:
             ft.set_opacity(1)
             ft2.set_opacity(1)
             self.wait(1)
 
-        self.play(ft2[ias:ias+10].animate.set(color="#E02A20").set_style(stroke_width=1.2))
+        self.play(ft2[ias:ias+10].animate.set(color="#E02A20").set_style(stroke_width=1.2), run_time=1)
         self.play(ft.animate.set_opacity(0), ft2[:ias].animate.set_opacity(0), ft2[ias+10:].animate.set_opacity(0),
                   monkey.animate.set_opacity(0), run_time=3)
-        self.play(ft2.animate.set(font_size=100).shift(RIGHT*2.6))
+        ft2.generate_target().set(font_size=100).shift(-ft2.target[ias:ias+10].get_center())
+        self.play(MoveToTarget(ft2), run_time=1)
         self.wait(1)
 
 
@@ -2948,6 +2952,6 @@ class AliceBob(AbraHT):
 
 
 if __name__ == "__main__":
-    with tempconfig({"quality": "low_quality", "preview": True}):
-        AbraGF().render()
+    with tempconfig({"quality": "high_quality", "preview": True}):
+        MonkeyType().render()
 #    print(SequenceH.sequences(10))
